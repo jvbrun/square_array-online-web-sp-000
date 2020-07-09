b6d16a3c5e5aba76c1f414bf92759ea9ef712d5c
Jean-Victor Brun <jeanvbrun@icloud.com>
2020-07-09 04:49:34 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,7 @@
-require_relative '../spec_helper.rb'
-require_relative '../square_array_spec.rb'
+array = [1, 2, 3]
 
 def square_array(array)
   array.each do |number|
     new_number = number * number 
-    printf {"When #{number} is squared it equals to #{new_number}."}
+    puts "When #{number} is squared it equals to #{new_number}."
 end
