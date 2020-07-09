1ed19976641ffc2b64f8e5123bdd3cc8d6f327c9
Jean-Victor Brun <jeanvbrun@icloud.com>
2020-07-09 04:54:58 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,7 @@ require_relative '../spec_helper.rb'
 require_relative '../square_array'
 
 def square_array(array)
-  array.each do |number|
-    new_number = number * number 
-    puts "When #{number} is squared it equals to #{new_number}."
+  array.each do |numbers|
+    numbers = numbers * numbers 
+    puts "When #{numbers}"
 end
